5b42d40f60050f23c434b5d1444f7d00343282f4
dallasmarin <31452075+dallasmarin@users.noreply.github.com>
2017-12-16 19:46:40 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,76 +5,131 @@
  * Description: This program  prints out a bank statement
  * */
 #include<iostream>
-#include<string>
 #include<vector>
 
-using namespace std;
 int main()
+
 {
-    float d,a,average = 0, minimum = 0, balance = 1143.24;
-    float balance2 = 1143.24;
-    float min_interest = 0, avg_interest = 0;
 
-    string s;
-    int j = 0;
-    bool b = true;
+int total_months;
 
-    vector <int> day;//three vectors store day, amount, and description
-    vector <float> amount;
-    vector <string> description;
+int day;
 
-    day.push_back(1);//stores initial or first entry in vector
-    amount.push_back(1143.24);
-    description.push_back("initial balance");
-    
-    cout << "press 0 to stop giving inputs\n";
-    cout << "enter inputs in form: day amount description\n\n";
+std::string description;
 
-    while(b)
-    {
-        cin >> d;//gets day from user
-        if(d==0)
-        {
-            b = false;
-        }
-        else
-        {
-            cin >> a;//gets amount from user
-            getline(cin,s);
-            day.push_back(a);
-            description.push_back(s);
-        }
-    }
-    cout << "\n";
-    cout << "Day\tAmount\tDescription\tBalance" << endl;
+float amount;
 
-    for(int i=0;i<30;i++)
-    {
-        if(i+1 == day[j])
-        {
-            if(j!=0)
-            {
-                balance = balance + amount[j];
-            }
-            if(balance2>balance)
-            {
-                balance2=balance;
-            }
-            cout << day[j] << "\t" << amount[j] << description[j] << "\t\t" << balance << endl;
-            j++;
-        }
-        average = average + balance;//Calculate the average daily balance of 30 days
-        minimum = minimum + balance2;//calculate the minimum daily balance of 30 days
-        balance2 = balance;
-    }
+float Average_daily_balance;
 
-    float apr = 0.5/100;//Calculates the rate of interest
-    float cycle = (float)30/365;//Calculates the cycle for 30 days to find the minimum and average daily balance interest.
-    min_interest = apr * cycle * minimum;//Calculate average and minimum daily balance interest.
-    avg_interest = apr * cycle * average;
-    cout << "\n";
-    cout << "Interest according to minimum daily balance: " << min_interest << endl;//Display average and minimum daily balance interest
-    cout << "Interest according to average daily balance: " << avg_interest << endl;
+float minimum_daily_balance = 100.00;
 
-    return 0;
+float penalty = 5.00;
+
+float balance;
+
+float deposit;
+
+float withdrawal;
+
+std::cout << "Enter Total number of months : ";
+
+std::cin >> total_months;
+
+std::cout << "\n\nEnter intial day, intial balance and Description : \n\n"; /* enter 1 for intial day for the starting month, then enter intial balance and intial description as given in the example */
+
+std::cin >> day >> balance >> description;
+
+for (int q = 1; q <= total_months; ++q)
+
+{
+
+std::cout << "\n" << q << " Month start : \n";
+
+std::cout << "\nEnter the transaction details day by day for the month " << q << " : ";
+
+std::vector<float> dayVector;
+
+std::vector<float> amountVector;
+
+std::vector<std::string> desVector;
+
+for (int i = 0; i < 28; i++)
+
+{
+
+std::cout << "\nEnter day : ";
+
+std::cin >> day;
+
+std::cout << "Enter the amount : ";
+
+std::cin >> amount;
+
+std::cout << "Enter the description : ";
+
+std::cin >> description;
+
+dayVector.push_back(day);
+
+amountVector.push_back(amount);
+
+desVector.push_back(description);
+
+}
+
+for (int i = 0; i < 28; i++)
+
+{ 
+//2 as day 1 already taken care of above
+
+deposit = 0;
+
+withdrawal = 0;
+
+balance += amount;
+
+if (amountVector[i] >= 0)
+
+{
+
+deposit = amountVector[i];
+
+}
+
+else
+
+{
+
+withdrawal = amountVector[i];
+
+}
+
+if (balance < minimum_daily_balance)
+
+{
+
+balance -= penalty;
+
+}
+
+Average_daily_balance += balance;
+
+std::cout << "\nDEPOSITS\t WITHDRAWALS\t BALANCE FOR THE DAY " << dayVector[i] << " :\n";
+
+std::cout << deposit << "\t \t " << withdrawal << "\t \t " << balance << "\n";
+
+std::cout << "\nMinimum daily balance = " << minimum_daily_balance << " and Average daily balance = " << Average_daily_balance << "\n";
+
+}
+
+// end of 30 days
+
+Average_daily_balance /= 30;
+
+balance += Average_daily_balance * 0.5;
+
+std::cout << "\n\n Interest calculated after month " << q << " = " << Average_daily_balance * 0.5;
+
+}
+
 }
